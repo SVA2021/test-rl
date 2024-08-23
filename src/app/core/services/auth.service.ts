d69d73f22d4420d2d3f1a5572de6962f892a32a2
VS <sofronov.vit@gmail.com>
2024-08-23 15:52:17 +0400

    add auth service logout

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { User } from '../models/models';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private user: User | null = null;
+
+  router = inject(Router);
 
   getUser(): User | null {
     return this.user;
@@ -17,5 +20,10 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return !!this.user;
+  }
+
+  logout() {
+    this.user = null;
+    this.router.navigate(['/login']).then();
   }
 }
