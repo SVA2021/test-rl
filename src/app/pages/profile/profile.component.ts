fbf7e5d3d8ae1d4ed882aed7f51ff02512741942
VS <sofronov.vit@gmail.com>
2024-08-23 18:21:10 +0400

    profile page view updated

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { User } from '../../core/models/models';
+import { TuiButton } from '@taiga-ui/core';
+import { RouterLink } from '@angular/router';
+
+const testUser: User = {
+  id: 'uuid',
+  username: 'test',
+  password: 'test',
+  is_online: true,
+};
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [TuiButton, RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent {}
+export class ProfileComponent implements OnInit {
+  user: User | null = null;
+
+  ngOnInit() {
+    this.user = testUser;
+  }
+}
