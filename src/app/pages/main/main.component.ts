3681b83cc81763955592b29a3002884c789bdc45
VS <sofronov.vit@gmail.com>
2024-08-23 14:18:12 +0400

    fix missed onpush

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.less'
+  styleUrl: './main.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainComponent {
 
