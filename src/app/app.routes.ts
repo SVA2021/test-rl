f1970194a7e82a5a615b376e29de816f0b88d557
VS <sofronov.vit@gmail.com>
2024-08-23 13:56:50 +0400

    add pages initial components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/main/main.component').then((m) => m.MainComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.component').then((m) => m.ProfileComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+  }
+];
