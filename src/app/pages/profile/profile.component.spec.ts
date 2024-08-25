435922ab5f5acfd92db71b5072347bda76b9be23
VS <sofronov.vit@gmail.com>
2024-08-25 22:37:09 +0400

    fix test profile

--- src/app/pages/profile/profile.component.spec.ts
+++ src/app/pages/profile/profile.component.spec.ts
@@ -2,15 +2,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
 import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
-
+  const mockActivatedRoute = {
+    params: of({ id: '123' }),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProfileComponent],
-      providers: [ActivatedRoute],
+      providers: [{ provide: ActivatedRoute, useValue: mockActivatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProfileComponent);
