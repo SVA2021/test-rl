cb31d1a9fc4deb22c39184854c70ebf9d5d6aeea
VS <sofronov.vit@gmail.com>
2024-08-25 21:41:43 +0400

    fix users list

--- src/app/components/channel-users-list/channel-users-list.component.ts
+++ src/app/components/channel-users-list/channel-users-list.component.ts
@@ -9,20 +9,20 @@ import {
   TuiDataListComponent,
   TuiDialogContext,
   TuiDialogService,
-  TuiLoader
+  TuiLoader,
 } from '@taiga-ui/core';
-import { combineLatest, map, ReplaySubject, takeUntil } from 'rxjs';
+import { combineLatest, map, Observable, of, ReplaySubject, switchMap, takeUntil } from 'rxjs';
 import { ChatChannel, ChatUser, User, UserChannel } from '@core/models/models';
 import { selectUsers } from '@store/users/users.selectors';
 import { selectUsersByChannelId } from '@store/user-channels/user-channels.selectors';
-import { AsyncPipe, NgForOf, NgIf, TitleCasePipe, UpperCasePipe } from "@angular/common";
-import { TuiAvatar } from "@taiga-ui/kit";
-import { TuiSelectModule, TuiTextfieldControllerModule } from "@taiga-ui/legacy";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
-import type { PolymorpheusContent } from "@taiga-ui/polymorpheus";
-import { TuiContext, tuiPure, TuiStringHandler } from "@taiga-ui/cdk";
-import { UserChannelsActions } from "@store/user-channels/user-channels.actions";
-import { ChannelsActions } from "@store/channels/channels.actions";
+import { AsyncPipe, NgForOf, NgIf, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { TuiAvatar } from '@taiga-ui/kit';
+import { TuiSelectModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import type { PolymorpheusContent } from '@taiga-ui/polymorpheus';
+import { TuiContext, TuiLet, tuiPure, TuiStringHandler } from '@taiga-ui/cdk';
+import { UserChannelsActions } from '@store/user-channels/user-channels.actions';
+import { ChannelsActions } from '@store/channels/channels.actions';
 
 @Component({
   selector: 'app-channel-users-list',
@@ -40,6 +40,7 @@ import { ChannelsActions } from "@store/channels/channels.actions";
     TuiSelectModule,
     ReactiveFormsModule,
     TuiTextfieldControllerModule,
+    TuiLet,
   ],
   templateUrl: './channel-users-list.component.html',
   styleUrl: './channel-users-list.component.less',
@@ -54,33 +55,32 @@ export class ChannelUsersListComponent implements OnInit, OnDestroy {
   private readonly dialogs = inject(TuiDialogService);
   private readonly destroy$ = new ReplaySubject(1);
 
+  activeChannel$ = this.activeDataService.getActiveChannel().pipe(takeUntil(this.destroy$));
   private activeChannel: ChatChannel | null = null;
   activeUser: User | null = this.authService.getUser();
-  private fullUsers$ = this.store.select(selectUsers).pipe(takeUntil(this.destroy$));
-  private allChannelUsers$ = this.store
-    .select(selectUsersByChannelId(this.activeChannel?.id))
-    .pipe(takeUntil(this.destroy$));
-
-  activeChannelUsers$ = combineLatest([this.allChannelUsers$, this.fullUsers$]).pipe(
-    takeUntil(this.destroy$),
-    map(([channelUsers, users]) => this.getFilteredUsers(channelUsers, users, true)),
-  );
-  nonActiveChannelUsers$ = combineLatest([this.allChannelUsers$, this.fullUsers$]).pipe(
-    takeUntil(this.destroy$),
-    map(([channelUsers, users]) => this.getFilteredUsers(channelUsers, users, false)),
-  );
   newUserControl = new FormControl<string | null>(null);
+  private fullUsers$ = this.store.select(selectUsers).pipe(takeUntil(this.destroy$));
+  private allChannelUsers$: Observable<UserChannel[]> = of([]);
+  activeChannelUsers$: Observable<ChatUser[]> = of([]);
+  nonActiveChannelUsers$: Observable<ChatUser[]> = of([]);
 
   ngOnInit() {
     this.store.dispatch(ChannelsActions.fetchChannels());
     this.store.dispatch(UserChannelsActions.fetchUserChannels());
-
-    this.activeDataService
-      .getActiveChannel()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((channel) => {
-        this.activeChannel = channel;
-      });
+    this.allChannelUsers$ = this.activeChannel$.pipe(
+      switchMap((channel) => this.store.select(selectUsersByChannelId(channel?.id))),
+    );
+    this.activeChannelUsers$ = combineLatest([this.allChannelUsers$, this.fullUsers$]).pipe(
+      takeUntil(this.destroy$),
+      map(([channelUsers, users]) => this.getFilteredUsers(channelUsers, users, true)),
+    );
+    this.nonActiveChannelUsers$ = combineLatest([this.allChannelUsers$, this.fullUsers$]).pipe(
+      takeUntil(this.destroy$),
+      map(([channelUsers, users]) => this.getFilteredUsers(channelUsers, users, false)),
+    );
+    this.activeChannel$.subscribe((channel) => {
+      this.activeChannel = channel;
+    });
   }
 
   ngOnDestroy() {
